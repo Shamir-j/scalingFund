0c4aa857e9d4c4e3d30bbc3e2cacafcc2e9ee3b9
SHAMIR JAMAL <gideonochiengokwach@gmail.com>
2022-01-17 11:32:13 +0300

    added terraform backend to plan and apply from terraform cloud

--- provider.tf
+++ provider.tf
@@ -20,15 +20,15 @@ terraform {
   }
 }
 
-# terraform {
-#   cloud {
-#     organization = "Zaam-Technologies"
+terraform {
+  cloud {
+    organization = "Zaam-Technologies"
 
-#     workspaces {
-#       name = "ScalingFund"
-#     }
-#   }
-# }
+    workspaces {
+      name = "ScalingFund"
+    }
+  }
+}
 
 
 data "google_client_config" "provider" {}
@@ -46,84 +46,3 @@ provider "kubernetes" {
   #   google_container_cluster.gke.master_auth[0].client_key
   # )
 }
-
-# resource "google_compute_address" "public_lb_ip" {
-#   name   = "example-lb-ip"
-#   region = local.region
-# }
-
-# resource "kubernetes_service" "app" {
-#   metadata {
-#     name = "app"
-#   }
-
-#   spec {
-#     selector = {
-#       run = "app"
-#     }
-
-#     session_affinity = "None"
-
-#     port {
-#       protocol    = "TCP"
-#       port        = 80
-#       target_port = 8080
-#     }
-
-#     type             = "LoadBalancer"
-#     load_balancer_ip = google_compute_address.public_lb_ip.address
-#   }
-# }
-
-# resource "kubernetes_deployment" "app" {
-#   metadata {
-#     name = "app"
-
-#     labels = {
-#       run = "app"
-#     }
-#   }
-
-#   spec {
-#     replicas = 1
-
-#     strategy {
-#       type = "RollingUpdate"
-
-#       rolling_update {
-#         max_surge       = 1
-#         max_unavailable = 0
-#       }
-#     }
-
-#     selector {
-#       match_labels = {
-#         run = "app"
-#       }
-#     }
-
-#     template {
-#       metadata {
-#         name = "app"
-#         labels = {
-#           run = "app"
-#         }
-#       }
-
-#       spec {
-#         container {
-#           image = "escoto/kotlinresthello"
-#           name  = "app"
-
-#           port {
-#             container_port = 8080
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
-# output "public_url" {
-#   value = "http://${google_compute_address.public_lb_ip.address}"
-# }
